64887808b0c1119c560fa5674f58121a4cc7ff33
Dennis Luc <dennisluc@Denniss-MacBook-Pro.local>
2021-05-23 15:03:20 -0700

    Finished Lab 8

--- __tests__/router.test.js
+++ __tests__/router.test.js
@@ -1,4 +1,27 @@
+import { TestScheduler } from '@jest/core';
+import { pushToHistory } from '../scripts/router.js';
+
 /**
  * @jest-environment jsdom
  */
+
+describe('test pushToHistory', () => {
+    test('settings case', () => {
+        const currState = pushToHistory('settings', 0);
+        expect(currState.length).toBe(window.history.length);
+        expect(currState.state.page).toBe('settings');
+    });
+
+    test('entry case', () => {
+        const currState = pushToHistory('entry', 3);
+        expect(currState.length).toBe(window.history.length);
+        expect(currState.state.page).toBe('entry3');
+    });
+
+    test('default case', () => {
+        const currState = pushToHistory('', 0);
+        expect(currState.length).toBe(window.history.length);
+        expect(currState.state.page).toBe(undefined);
+    });
+});
 
